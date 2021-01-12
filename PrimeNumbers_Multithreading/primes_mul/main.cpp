1b2d97cdce45855712f978f00d89814e4e6c3288
Marc-Olivier Andrez <marc-olivier@peregrine.ai>
2021-01-12 19:52:34 +0100

    Changes done during the pair-programming session

--- PrimeNumbers_Multithreading/primes_mul/main.cpp
+++ PrimeNumbers_Multithreading/primes_mul/main.cpp
@@ -1,7 +1,7 @@
 //*************************************************************************
 // Code: Generator of Prime Numbers with Multithreading
 // Author: Shambhavi K.
-// Date: 08.01.2021 (Last updated: 09.01.2021)      
+// Date: 08.01.2021 (Last updated: 09.01.2021)
 //*************************************************************************
 
 #include <iostream>
@@ -17,26 +17,34 @@
 using namespace std;
 using namespace std::chrono;
 
-vector<unsigned int> vectPrime;		//global vector
-mutex mutexLock;
+bool isPrime(unsigned int i)
+{
+  for (unsigned int j = 2; j <= sqrt(i); j++)
+  {
+    bool isIMultipleOfJ = (i % j) == 0;
+    if (isIMultipleOfJ)
+    {
+      return false;
+    }
+  }
+  return true;
+}
 
 //*************************************************************************
 // Function: calculatePrimeNumbers()
 // Description: calculation of prime numbers using Trial Division method
 //*************************************************************************
 
-void calculatePrimeNumbers(unsigned int firstNumber, unsigned int lastNumber) {
-	for(unsigned int i=firstNumber; i<=lastNumber; i+=2) {
-				for(unsigned int j=2; j<i; j++) {
-					if((i%j) == 0) { break; }
-					else if((j+1)==i) {
-						mutexLock.lock();
-						vectPrime.push_back(i);
-						mutexLock.unlock();
-					}
-				}
-			}
-} 
+void calculatePrimeNumbers(vector<unsigned int> &vectPrime, unsigned int firstNumber, unsigned int lastNumber)
+{
+  for (unsigned int i = firstNumber; i <= lastNumber; i += 2)
+  {
+    if (isPrime(i))
+    {
+      vectPrime.push_back(i);
+    }
+  }
+}
 
 //*************************************************************************
 // Function: primeNumbersWithThreads()
@@ -44,77 +52,90 @@ void calculatePrimeNumbers(unsigned int firstNumber, unsigned int lastNumber) {
 //              prime numbers upto N.
 //*************************************************************************
 
-void primeNumbersWithThreads(unsigned int firstNumber, 
-						unsigned int lastNumber, unsigned int totalThreads) 
+std::vector<unsigned int> primeNumbersWithThreads(unsigned int firstNumber,
+                                                  unsigned int lastNumber, unsigned int totalThreads)
 {
-	unsigned int threadSpread = lastNumber / totalThreads;
-	unsigned int newLastNumber = firstNumber + threadSpread - 1;
-	
-			//create different thread object 
-			thread thread1(calculatePrimeNumbers, firstNumber, newLastNumber); //thread object
-			firstNumber = firstNumber + threadSpread;
-			newLastNumber = newLastNumber + threadSpread;
-			
-			thread thread2(calculatePrimeNumbers, firstNumber, newLastNumber); //thread object
-			firstNumber = firstNumber + threadSpread;
-			newLastNumber = newLastNumber + threadSpread;
-			
-			thread thread3(calculatePrimeNumbers, firstNumber, newLastNumber); //thread object
-	
-	thread1.join();
-	thread2.join();
-	thread3.join();
+  unsigned int threadSpread = lastNumber / totalThreads;
+  unsigned int newLastNumber = firstNumber + threadSpread - 1;
+
+  //create different thread object
+  vector<unsigned int> vectPrime1;
+  thread thread1(calculatePrimeNumbers, std::ref(vectPrime1), firstNumber, newLastNumber); //thread object
+  firstNumber = firstNumber + threadSpread;
+  newLastNumber = newLastNumber + threadSpread;
+
+  vector<unsigned int> vectPrime2;
+  thread thread2(calculatePrimeNumbers, std::ref(vectPrime2), firstNumber, newLastNumber); //thread object
+  firstNumber = firstNumber + threadSpread;
+  newLastNumber = newLastNumber + threadSpread;
+
+  vector<unsigned int> vectPrime3;
+  thread thread3(calculatePrimeNumbers, std::ref(vectPrime3), firstNumber, newLastNumber); //thread object
+
+  thread1.join();
+  thread2.join();
+  thread3.join();
+
+  vectPrime1.insert(vectPrime1.end(), vectPrime2.cbegin(), vectPrime2.cend());
+  vectPrime1.insert(vectPrime1.end(), vectPrime3.cbegin(), vectPrime3.cend());
+
+  return vectPrime1;
 }
 
 //*************************************************************************
 // Function: validInput()
-// Description: Checks validity of the user-entered input. If anything 
-//              apart from numeric is entered, the function ignores the 
+// Description: Checks validity of the user-entered input. If anything
+//              apart from numeric is entered, the function ignores the
 //              data and prompts the user to enter postive integer value.
 //*************************************************************************
 
 int validInput()
 {
-    int input;
+  int input;
+  cin >> input;
+  while (cin.fail())
+  {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Incorrect entry. Please enter a positive integer = ";
     cin >> input;
-    while(cin.fail())
-    {
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(),'\n');
-        cout << "Incorrect entry. Please enter a positive integer = ";
-        cin >> input;
-    }
-    return input;
+  }
+  return input;
 }
 
 //*************************************************************************
 // Function: main()
-// Description: Accepts data from the user and processes generation of 
+// Description: Accepts data from the user and processes generation of
 //              prime numbers. Addition of benchmark conditions (clocks) to
-//              observe timing behaviour of algorithm (in microseconds). 
+//              observe timing behaviour of algorithm (in microseconds).
 //*************************************************************************
 
-int main() {
-	unsigned int N = 0;
-	
-	cout << "Enter a positive integer =";
-	N = validInput();
-	
-	if(N==1) cout << "1 is neither prime nor composite"; 
-	
-	//vector<unsigned int> vectPrime(N,1);
-	
-	auto start = high_resolution_clock::now();
-		primeNumbersWithThreads(1, N, 3);
-	auto stop = high_resolution_clock::now();
+int main()
+{
+  cout << "Enter a positive integer =";
+  // int: -1 = 0xffff
+  // unsigned int: 0xffff = 2^32 - 1
+  unsigned int N = validInput();
+  cout << "N=" << N << endl;
 
-	cout << "2 ";	//hardcoded value of 2
-	
-	for(auto i: vectPrime) {
-		cout << i << " ";
-	  }
-	
-	auto duration = duration_cast<microseconds>(stop - start);
-	cout << "\nTotal Time taken = " << duration.count() 
-					<< " microseconds" << endl;
+  if (N == 1)
+    cout << "1 is neither prime nor composite";
+
+  //vector<unsigned int> vectPrime(N,1);
+
+  auto start = steady_clock::now();
+  // vectPrime.reserve(N);
+  std::vector<unsigned int> vectPrime = primeNumbersWithThreads(1, N, 3);
+  auto stop = steady_clock::now();
+
+  // cout << "2 "; //hardcoded value of 2
+
+  // for (auto i : vectPrime)
+  // {
+  //   cout << i << " ";
+  // }
+
+  auto duration = duration_cast<microseconds>(stop - start);
+  cout << "\nTotal Time taken = " << duration.count()
+       << " microseconds" << endl;
 }
--- PrimeNumbers_SOE/primes_SOE/main.cpp
+++ PrimeNumbers_SOE/primes_SOE/main.cpp
@@ -1,7 +1,7 @@
 //*************************************************************************
 // Code: Generator of Prime Numbers using algorithm "Seive of Eratosthenes"
 // Author: Shambhavi K.
-// Date: 03.01.2021 (Last updated: 10.01.2021)      
+// Date: 03.01.2021 (Last updated: 10.01.2021)
 //*************************************************************************
 
 #include <iostream>
@@ -15,66 +15,88 @@ using namespace std::chrono;
 
 //*************************************************************************
 // Function: findPrimeNumbers()
-// Description: Takes the value 'N' entered by the user and generates 
+// Description: Takes the value 'N' entered by the user and generates
 //              & displays prime numbers upto Nth value.
 //*************************************************************************
 
-void findPrimeNumbers(vector<int> vectorPrimes, int N) {
-	for (int i=2; i<=sqrt(N); i++) {
-        for (int j=i; (i*j)<N; j++) {
-				vectorPrimes[i*j] = 0;
-		}
-	}
-	
-	for(int k=0; k<vectorPrimes.size(); k++) {
-		if(vectorPrimes[k])		cout << k << " ";
-	}
+vector<bool> findPrimeNumbers(int N)
+{
+  vector<bool> vectorPrimes(N, false);
+  vectorPrimes[0] = 0;
+  vectorPrimes[1] = 0;
+  for (int i = 2; i <= sqrt(N); ++i)
+  {
+    for (int j = i; (i * j) < N; ++j)
+    {
+      vectorPrimes[i * j] = true;
+    }
+  }
+
+  // for (int k = 0; k < vectorPrimes.size(); k++)
+  // {
+  //   if (vectorPrimes[k])
+  //     cout << k << " ";
+  // }
+  return vectorPrimes;
 }
 
 //*************************************************************************
 // Function: validInput()
-// Description: Checks validity of the user-entered input. If anything 
-//              apart from numeric is entered, the function ignores the 
+// Description: Checks validity of the user-entered input. If anything
+//              apart from numeric is entered, the function ignores the
 //              data and prompts the user to enter postive integer value.
 //*************************************************************************
 
-int validInput() {
-    int input;
+int validInput()
+{
+  int input;
+  cin >> input;
+  while (cin.fail())
+  {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Incorrect entry. Please enter a positive integer: ";
     cin >> input;
-    while(cin.fail()) {
-        cin.clear();
-        cin.ignore(numeric_limits <streamsize>::max(),'\n');
-        cout << "Incorrect entry. Please enter a positive integer: ";
-        cin >> input;
-    }
-    return input;
+  }
+  return input;
 }
 
 //*************************************************************************
 // Function: main()
-// Description: Accepts data from the user and processes generation of 
+// Description: Accepts data from the user and processes generation of
 //              prime numbers. Addition of benchmark conditions (clocks) to
-//              observe timing behaviour of algorithm (in microseconds). 
+//              observe timing behaviour of algorithm (in microseconds).
 //*************************************************************************
 
-int main() {
-	unsigned int N = 0;
-	
-	cout << "Enter a positive integer =";
-	N = validInput();
-	
-	//precaution
-	if(N==1) cout << "1 is neither prime nor composite"; 
-	if(N<=0) cout << "Please enter a positive integer";
-	
-	vector<int> vectorPrimes(N,1);
-	vectorPrimes[0] = 0; vectorPrimes[1] = 0;
-	 
-	auto start = high_resolution_clock::now();
-		findPrimeNumbers(vectorPrimes, N);
-	auto stop = high_resolution_clock::now();
-	
-	auto duration = duration_cast<microseconds>(stop - start);
-	cout << "\nTime taken by function: " << 
-				duration.count() << " microseconds" << endl;
+int main()
+{
+  unsigned int N = 0;
+
+  cout << "Enter a positive integer =";
+  N = validInput();
+
+  //precaution
+  if (N == 1)
+    cout << "1 is neither prime nor composite";
+  if (N <= 0)
+    cout << "Please enter a positive integer";
+
+  // vector<int> vectorPrimes(N, 1);
+  // vectorPrimes[0] = 0;
+  // vectorPrimes[1] = 0;
+
+  auto start = steady_clock::now();
+  vector<bool> vectorPrimes = findPrimeNumbers(N);
+  auto stop = steady_clock::now();
+
+  auto duration = duration_cast<microseconds>(stop - start);
+  cout << "\nTime taken by function: " << duration.count() << " microseconds" << endl;
+
+  // cout << "\n";
+  // for (int k = 0; k < vectorPrimes.size(); k++)
+  // {
+  //   if (vectorPrimes[k])
+  //     cout << k << " ";
+  // }
+  // cout << "\n";
 }
